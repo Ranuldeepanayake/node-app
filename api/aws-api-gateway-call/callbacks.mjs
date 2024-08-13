63095959ef106ff3c879f51e98d85e8147d1273e
Ranul Deepanayake <ranuldeepanayake@outlook.com>
2024-08-13 13:43:15 +0530

    API testing

---
+++ api/aws-api-gateway-call/callbacks.mjs
@@ -0,0 +1,11 @@
+export function callBack1(arg1){
+    console.log("This is callback1");
+    arg1();
+    console.log("This is callback1");
+}
+
+export function function1(){
+    console.log("This is function1");
+}
+
+
